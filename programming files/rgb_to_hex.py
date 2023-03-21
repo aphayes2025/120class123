d1030bccd9782f61623047b1f6e10c46d71b39f0
Aidan Hayes <112426576+aphayes2025@users.noreply.github.com>
2023-03-21 16:42:51 -0400

    Update rgb_to_hex.py
    Added descriptive comments

--- programming files/rgb_to_hex.py
+++ programming files/rgb_to_hex.py
@@ -1,6 +1,10 @@
+# rgb to hex function
 def rgb_to_hex(r, g, b):
+    # red
     r = max(0, min(255, r))
+    # green
     g = max(0, min(255, g))
+    # blue
     b = max(0, min(255, b))
     return '{:02X}{:02X}{:02X}'.format(r, g, b)
 
